1c4ee3190de3dc8b8804e21d474fff6d5bd68d65
Bas van Driel <contact@basvandriel.nl>
2023-01-03 20:58:26 +0100

    sql support

--- src/abn_assignment/api/__init__.py
+++ src/abn_assignment/api/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
 
+from abn_assignment.constants import (
+    DATABASE_HOST,
+    DATABASE_NAME,
+    DATABASE_PASSWORD,
+    DATABASE_PORT,
+    DATABASE_USER,
+)
+from ..sql_database import database
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"""
+    postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}
+    """
+    database.init_app(app)
 
     @app.route("/")
     def _():
