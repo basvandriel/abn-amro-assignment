ee603b7690ccc5e3056edff3d5cc7193babd94bc
Bas van Driel <contact@basvandriel.nl>
2023-01-03 22:27:06 +0100

    yes

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session
 from abn_assignment.country import Country
 
 
-def test_hi(sql_session: scoped_session):
+def test_add_country(sql_session: scoped_session):
     country = Country("Nederland", "NL", 123.2, [])
     sql_session.add(country)
     sql_session.commit()
