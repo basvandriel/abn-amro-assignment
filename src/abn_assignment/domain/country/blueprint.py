80c0dd5ab263769b3d7a9e7a4f7f51d4d4a93eb5
Bas van Driel <contact@basvandriel.nl>
2023-01-04 18:45:13 +0100

    yes

--- src/abn_assignment/domain/country/blueprint.py
+++ src/abn_assignment/domain/country/blueprint.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
+from abn_assignment.domain.country.sql_repository import SQLCountryRepository
+from abn_assignment.domain.developer.sql_repository import (
+    SQLDeveloperRepository,
+)
+
+
 from abn_assignment.domain.country.service import CountryService
 
 from abn_assignment.sql_database import database
@@ -10,4 +16,8 @@ blueprint = Blueprint("domain", __name__)
 
 @blueprint.route("/youngest_coding_gdp/<iso_code>")
 def resolve_youngest_coding_gdp(iso_code: str) -> dict[str, str | float]:
-    return asdict(CountryService(database.session).get_youngest_gdp(iso_code))
+    service = CountryService(
+        SQLCountryRepository(database.session),
+        SQLDeveloperRepository(database.session),
+    )
+    return asdict(service.get_youngest_gdp(iso_code))
--- tests/test_get_youngest_gdp.py
+++ tests/test_get_youngest_gdp.py
@@ -1,10 +1,10 @@
 from flask.testing import FlaskClient
 
 
-def test_get_bad(client: FlaskClient):
+def test_get_gdp_no_data(client: FlaskClient):
     response = client.get("/youngest_coding_gdp/NL")
-
-    assert response.json == {
-        "gdp_in_euros": 893.0,
-        "youngest_age_range": "18",
-    }
+    response.status_code == 404
+    # assert response.json == {
+    #     "gdp_in_euros": 893.0,
+    #     "youngest_age_range": "18",
+    # }
