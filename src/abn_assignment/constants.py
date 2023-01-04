02f59d8406fdc33dbf488a964cbc50f5bc5050bb
Bas van Driel <contact@basvandriel.nl>
2023-01-04 20:26:57 +0100

    whoops

--- src/abn_assignment/constants.py
+++ src/abn_assignment/constants.py
@@ -5,7 +5,7 @@ from pathlib import Path
 DATABASE_USER: str = getenv("DATABASE_USER", "postgres")
 DATABASE_PASSWORD: str = getenv("DATABASE_PASSWORD", "")
 DATABASE_HOST: str = getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT: int = getenv("DATABASE_PORT", 5432)
+DATABASE_PORT: str = getenv("DATABASE_PORT", "5432")
 DATABASE_NAME: str = getenv("DATABASE_NAME", "abnamro")
 
 USING_YEAR_DATA: int = 2021
